a1c7d0eb2e899af51df59c20c79e3e48739683bd
Shintaro0806 <shin-taro@outlook.jp>
2025-02-03 17:11:16 +0900

    作り直し

---
+++ src/main/java/raisetech/Re/Student/Management/domain/StudentDetail.java
@@ -0,0 +1,25 @@
+package raisetech.Re.Student.Management.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import raisetech.Re.Student.Management.data.Student;
+import raisetech.Re.Student.Management.data.StudentCourse;
+
+@Schema(description = "受講生詳細")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDetail {
+
+  @Valid
+  private Student student;
+
+  @Valid
+  private List<StudentCourse> studentCourseList;
+}
