7b6e04ead77438a4950d465fd464f75b934bceb4
Shintaro0806 <shin-taro@outlook.jp>
2025-02-04 10:34:53 +0900

    エラー

--- src/main/java/raisetech/Re/Student/Management/service/Application.java
+++ src/main/java/raisetech/Re/Student/Management/service/Application.java
@@ -1,9 +1,13 @@
 package raisetech.Re.Student.Management.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("raisetech.Re.Student.Management.repository")
+@ComponentScan(basePackages = "raisetech.Re.Student.Management")
 public class Application {
 
 	public static void main(String[] args) {
