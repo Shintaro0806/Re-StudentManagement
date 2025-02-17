f7a9f88647c85fc52d07be08854ae20ecd4e758a
Shintaro0806 <shin-taro@outlook.jp>
2025-02-17 12:22:27 +0900

    スタンダード第30回のrepositoryのテストを作っていたのですが、エラーが起きてしまいます。

---
+++ src/test/java/raisetech/Re/Student/Management/repository/StudentRepositoryTest.java
@@ -0,0 +1,43 @@
+package raisetech.Re.Student.Management.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import raisetech.Re.Student.Management.data.Student;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Autowired
+  private StudentRepository sut;
+
+  @Test
+  void 受講生の全件検索ができること() {
+    List<Student> actual = sut.search();
+    assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生の登録が行えること() {
+    Student student = new Student();
+    student.setName("江波浩二");
+    student.setKanaName("エナミコウジ");
+    student.setNickname("エナミ");
+    student.setEmail("test@example.com");
+    student.setArea("奈良県");
+    student.setAge(36);
+    student.setSex("男性");
+    student.setRemark("");
+    student.setDeleted(false);
+
+    sut.registerStudent(student);
+
+    List<Student> actual = sut.search();
+    assertThat(actual.size()).isEqualTo(6);
+  }
+  
+}
