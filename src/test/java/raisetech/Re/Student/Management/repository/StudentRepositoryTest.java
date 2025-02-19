a1b78d9dfca2ff015af50fabd87df3325ab1e6da
Shintaro0806 <shin-taro@outlook.jp>
2025-02-19 11:54:55 +0900

    スタンダード第30回演習課題です。repositoryのテストを作りました。 以前、質問投稿で、data.sqlとapplication.propertiesとschema.sqlをあげているため、課題の範囲すべてのデータではないかもしれませんが、よろしくお願いします。

--- src/test/java/raisetech/Re/Student/Management/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/Re/Student/Management/repository/StudentRepositoryTest.java
@@ -1,15 +1,20 @@
 package raisetech.Re.Student.Management.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import raisetech.Re.Student.Management.data.Student;
+import raisetech.Re.Student.Management.data.StudentCourse;
+import raisetech.Re.Student.Management.service.Application;
 
 @MybatisTest
+@ContextConfiguration(classes = Application.class)
 class StudentRepositoryTest {
 
   @Autowired
@@ -18,7 +23,57 @@ class StudentRepositoryTest {
   @Test
   void 受講生の全件検索ができること() {
     List<Student> actual = sut.search();
+
     assertThat(actual.size()).isEqualTo(5);
+  }
+
+  @Test
+  void 受講生をIDで検索ができること() {
+    Student actual = sut.searchStudent("1");
+
+    assertThat(actual).isNotNull();
+    assertThat(actual.getId()).isEqualTo("1");
+    assertThat(actual.getName()).isEqualTo("山田太郎");
+    assertThat(actual.getKanaName()).isEqualTo("ヤマダタロウ");
+    assertThat(actual.getNickname()).isEqualTo("タロ");
+    assertThat(actual.getEmail()).isEqualTo("taro@example.com");
+    assertThat(actual.getArea()).isEqualTo("東京");
+    assertThat(actual.getAge()).isEqualTo(25);
+    assertThat(actual.getSex()).isEqualTo("男性");
+  }
+
+  @Test
+  void 受講生のコース情報の全件検索ができること() {
+    List<StudentCourse> actual = sut.searchStudentCourseList();
+
+    assertThat(actual.size()).isEqualTo(10);
+  }
+
+  @Test
+  void 受講生のコース情報をIDで検索ができること() {
+    List<StudentCourse> actual = sut.searchStudentCourse("1");
+
+    assertThat(actual.size()).isEqualTo(3);
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    StudentCourse firstCourse = actual.get(0);
+    assertThat(firstCourse.getStudentId()).isEqualTo("1");
+    assertThat(firstCourse.getCourseName()).isEqualTo("Javaコース");
+    assertThat(firstCourse.getCourseStartAt().format(formatter)).isEqualTo("2023-04-01T09:00:00");
+    assertThat(firstCourse.getCourseEndAt().format(formatter)).isEqualTo("2023-07-01T15:00:00");
+
+    StudentCourse secondCourse = actual.get(1);
+    assertThat(secondCourse.getStudentId()).isEqualTo("1");
+    assertThat(secondCourse.getCourseName()).isEqualTo("AWSコース");
+    assertThat(secondCourse.getCourseStartAt().format(formatter)).isEqualTo("2023-05-01T10:00:00");
+    assertThat(secondCourse.getCourseEndAt().format(formatter)).isEqualTo("2023-08-01T16:00:00");
+
+    StudentCourse thirdCourse = actual.get(2);
+    assertThat(thirdCourse.getStudentId()).isEqualTo("1");
+    assertThat(thirdCourse.getCourseName()).isEqualTo("Web制作コース");
+    assertThat(thirdCourse.getCourseStartAt().format(formatter)).isEqualTo("2024-01-01T13:00:00");
+    assertThat(thirdCourse.getCourseEndAt().format(formatter)).isEqualTo("2024-05-01T19:00:00");
   }
 
   @Test
@@ -39,5 +94,42 @@ class StudentRepositoryTest {
     List<Student> actual = sut.search();
     assertThat(actual.size()).isEqualTo(6);
   }
-  
+
+  @Test
+  void 受講生コース情報の登録が行えること() {
+    StudentCourse studentCourse = new StudentCourse();
+    studentCourse.setStudentId("2");
+    studentCourse.setCourseName("Spring Bootコース");
+    studentCourse.setCourseStartAt(LocalDateTime.now());
+    studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+
+    sut.registerStudentCourse(studentCourse);
+
+    List<StudentCourse> actual = sut.searchStudentCourseList();
+    assertThat(actual.size()).isEqualTo(11);
+  }
+
+  @Test
+  void 受講生の更新が行えること() {
+    Student actual = sut.searchStudent("1");
+    actual.setRemark("更新しました。");
+
+    sut.updateStudent(actual);
+
+    actual = sut.searchStudent("1");
+    assertThat(actual.getRemark()).isEqualTo("更新しました。");
+  }
+
+  @Test
+  void 受講生コース情報のコース名の更新が行えること() {
+    List<StudentCourse> actual = sut.searchStudentCourse("1");
+    StudentCourse studentCourse1 = actual.get(0);
+    studentCourse1.setCourseName("Javaアドバンスコース");
+
+    sut.updateStudentCourse(studentCourse1);
+
+    actual = sut.searchStudentCourse("1");
+    studentCourse1 = actual.get(0);
+    assertThat(studentCourse1.getCourseName()).isEqualTo("Javaアドバンスコース");
+  }
 }
